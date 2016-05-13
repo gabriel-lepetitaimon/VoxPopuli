98a7c59e80333f3fd6ff3fd97f83ce44023f8495
gabriel.lepetitaimon <gabriel.lepetitaimon@gmail.com>
2016-05-13 08:34:09 +0200

    The marvelous singleton!

--- src/lib/singleton.h
+++ src/lib/singleton.h
@@ -1,4 +1,36 @@
 #ifndef SINGLETON_H
 #define SINGLETON_H
 
+template<typename T> class singleton{
+public:
+    static T& instance(){
+        if(!_singleton)
+            init();
+        return &_singleton;
+    }
+
+    static bool init(){
+        if(_singleton)
+            return false;
+        _singleton = new T();
+        return true;
+    }
+    static void clean(){
+        if(!_singleton)
+            return;
+        delete _singleton;
+        _singleton = 0;
+    }
+
+    static T* ptr(){
+        return _singleton;
+    }
+
+private:
+    static T* _singleton;
+};
+
+template<typename T>
+T* singleton<T>::_singleton = 0;
+
 #endif // SINGLETON_H
